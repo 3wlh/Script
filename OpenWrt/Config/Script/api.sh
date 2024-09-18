f13ed30566bd4e2c75d170d886fe0800d5aaa09f
3wlh <1094890624@qq.com>
2024-09-19 00:27:54 +0800

    更新 api.sh

--- OpenWrt/Config/Script/api.sh
+++ OpenWrt/Config/Script/api.sh
@@ -6,7 +6,7 @@ api="https://gitee.com/git_3wlh/File/raw/main/OpenWrt/"
 # api="https://raw.githubusercontent.com/3wlh/Script/main/OpenWrt/Config/API/"
 Default_URL="https://ghproxy.net/https://raw.githubusercontent.com/3wlh/Script/main/OpenWrt/Config/Config/"
 PWD="${1:""}"
-URL="${2:${Default_URL}}"
+URL="${2:"${Default_URL}"}"
 
 #========函数========
 
