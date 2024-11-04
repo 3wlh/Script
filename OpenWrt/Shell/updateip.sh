0906337056528d9c7b4ea10085fa1e675f87b75e
3wlh <1094890624@qq.com>
2024-11-04 15:10:47 +0800

    更新 updateip.sh

--- OpenWrt/Shell/updateip.sh
+++ OpenWrt/Shell/updateip.sh
@@ -5,14 +5,15 @@ Cron_text=$(cat "/etc/crontabs/root" 2>/dev/null)
 test  -d ${File%/*} || mkdir -p ${File%/*}
 cat>$File<<EOF
 #/bin/bash
-# Webpage=\$(wget -qO - http://ip.3wlh.cn)
 log="/tmp/log/updateip.log"
 time=\$(date +"%Y/%m/%d %H:%M.%S")
+web=\$(http://ip.3wlh.cn)
+# Webpage=\$(wget -qO - http://ip.3wlh.cn)
 # ip=\$(wget -qO - http://members.3322.org/dyndns/getip)
 ip=\$(ubus call network.interface.wan status | grep \"address\" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 test  -d \${log%/*} || mkdir -p \${log%/*}
 if [ ! -n "\$(sed -n '\$p' \${log} 2>/dev/null | grep "\${ip}")" ]; then
-	result=\$(wget -qO - "http://ip.3wlh.cn/update.php?IP=\${ip}")
+	result=\$(wget -qO - "\${web}/update.php?IP=\${ip}")
 	if [ -n "\$(echo \${result} | grep "数据保存完成")" ]; then
 		echo "\${time} update:\${ip}" >> \${log}
 		echo "更新IP完成"
