679bbb2386f82c8a4079fb433e15443f5d9f0a0d
3wlh <1094890624@qq.com>
2024-11-25 23:03:25 +0800

    更新 Getmac.sh

--- OpenWrt/Shell/Getmac.sh
+++ OpenWrt/Shell/Getmac.sh
@@ -1,5 +1,5 @@
 #!/bind/bash
-if (-n "$(ip -o link show eth0 | grep "permaddr")");then
+if [ -n "$(ip -o link show eth0 | grep \"permaddr\")"];then
 	echo $(ip -o link show eth0 | awk '{print $NF}' | tr -d '\n' | md5sum | awk '{print $1}' | cut -c9-24)
 else
 	echo $(cat /sys/class/net/eth0/address | awk '{print $NF}' | tr -d '\n' | md5sum | awk '{print $1}' | cut -c9-24)
