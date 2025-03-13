857a44d83bc5f525ae9ade7d1593dc97d3854e04
3wlh <1094890624@qq.com>
2025-03-13 16:10:12 +0800

    Update RustDesk.sh

--- OpenWrt/Shell/RustDesk.sh
+++ OpenWrt/Shell/RustDesk.sh
@@ -6,5 +6,4 @@ dir="/etc/rustdesk-server/"
 mkdir -p ${dir}
 echo "aTxYkmuXWVBEEOdwiK2xpuUQ5YVFQQqxgeGdYbWd8uY=" > ${dir}id_ed25519.pub
 echo "5QToaNwZFgZRhvvD3rUYJQ2MweZUjSB8YH++TYo3YP5pPFiSa5dZUEQQ53CIrbGm5RDlhUVBCrGB4Z1htZ3y5g==" > ${dir}id_ed25519
-chown -R rustdesk:rustdesk ${dir}
 echo "配置完成"
