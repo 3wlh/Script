c332fb0790407e53a7262bd3b99c0c58de477026
3wlh <1094890624@qq.com>
2025-04-02 13:41:00 +0800

    Update API_qqpush.sh

--- OpenWrt/sh_api/API_qqpush.sh
+++ OpenWrt/sh_api/API_qqpush.sh
@@ -5,7 +5,7 @@ fi
 echo "开始推送消息"
 sleep 30s
 message="更新IP：$(ubus call network.interface.wan status | grep \"address\" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
-data=`curl --location --request POST "http://msg.3wlh.cn/send_private_msg" \
+data=`curl -s --location --request POST "http://msg.3wlh.cn/send_private_msg" \
 --header "Content-Type: application/json" \
 --data-raw "{\"user_id\":\"1094890624\",\"message\":[{\"type\":\"text\",\"data\":{\"text\":\"${message}\"}}]}"`
 echo ${data}
