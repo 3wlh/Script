ba137802b945c717a22366185768611e69dd6fbc
3wlh <1094890624@qq.com>
2026-02-03 19:46:16 +0800

    Update photopea.sh

--- Docker/photopea/photopea.sh
+++ Docker/photopea/photopea.sh
@@ -6,9 +6,15 @@ JS_File=$(find $(pwd)/photopea/code/pp/ -name *.js)
 sed -i "s/color:#ffffff; background-color:#/display:none; background-color:#/g" ${JS_File}
 sed -i "s/float:right.*index:1.*app-region:no-drag;/opacity:0; pointer-events:none;/g" ${JS_File}
 sed -i "s/padding: 8px.*13:36.*px; cursor:pointer;/display:none;/g" ${JS_File}
-# 直接跳转
-sed -i "s/function addPP()/window.onload = function()/g" $(pwd)/www.photopea.com/index.html
 (cd photopea && {
+# 修改html
+sed -i "s|img/table.webp||g" $(pwd)/index.html
+sed -i "s|img/hand_phone.webp||g" $(pwd)/index.html
+sed -i "s|img/goats.mp4||g" $(pwd)/index.html
+sed -i "s|img/laptop.webp||g" $(pwd)/index.html
+sed -i "s|addPP()||g" $(pwd)/index.html
+sed -i "s/function addPP()/window.onload = function()/g" $(pwd)/index.html
+# 添加修改JS	
 JS_DBS=$(find $(pwd)/code/dbs/ -name *.js)
 sed -i '/var FNTS = {/,/};/d' ${JS_DBS} && \
 wget -qO - http://script.11121314.xyz/Docker/photopea/function.js >> ${JS_DBS}
